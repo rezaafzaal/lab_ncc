a8d3007b318c9af28a1510e19f3b37d4bb268d2a
rezaafzaal <rezaafzaaltq@gmail.com>
2026-05-04 17:56:01 +0700

    hmm

--- test/math.test.js
+++ test/math.test.js
@@ -3,13 +3,13 @@ const { add, subtract, multiply, divide } = require('../src/math');
 describe('Math Utils', () => {
   describe('add()', () => {
     test('adds two positive numbers', () => {
-      expect(add(2, 3)).toBe(5);
+      expect(add(2, 3)).toBe(6);
     });
     test('adds negative numbers', () => {
-      expect(add(-1, -2)).toBe(-3);
+      expect(add(-1, -2)).toBe(-4);
     });
     test('adds zero', () => {
-      expect(add(5, 0)).toBe(5);
+      expect(add(5, 0)).toBe(50);
     });
   });
 
